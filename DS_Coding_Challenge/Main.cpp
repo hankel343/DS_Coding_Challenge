60777069c77554547bc572252b35fb9918b7b1ea
hankel343 <57358008+hankel343@users.noreply.github.com>
2021-03-29 10:53:04 -0500

    Updated 3/29/2021

--- DS_Coding_Challenge/Main.cpp
+++ DS_Coding_Challenge/Main.cpp
@@ -34,11 +34,13 @@ int main()
 
 	//Calculation is slowed down by prime numbers that have to have each number up to itself tested.
 	//Composite numbers take less time to find because as soon as a mod of 0 is found it is known that i is not a prime number.
+	//One approach that might cut down on computational time is using an array of found composite numbers to test i.
 
 	end = chrono::system_clock::now();
 	elapsed = end - start;
 
 	cout << "Calculation runtime: " << elapsed.count() << endl;
+	cout << nPrimes.size() << " primes were found.\n";
 
 	for (int i = 0; i < nPrimes.size(); i++)
 	{
